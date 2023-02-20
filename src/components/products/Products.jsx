9c0125bc2c81df3dd3417be37f597ef7dfe43d7f
jkunciene <j.kunciene@gmail.com>
2023-02-20 21:09:53 +0200

    ideti i krepseli patikrinimai

--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -5,7 +5,29 @@ const Products = () => {
   const [items, setItems] = useState([]);
 
   const addToCart = (item) => {
-    console.log('funkcija addToCart', item)
+    console.log('funkcija addToCart', item);
+    // jei tokios prekes dar nera krepselyje -> sukurk savybe kiekis 1
+    //jei tokia preke jau yra -> didink kieki +1
+    //jei prekes likutis pakankamas
+    //extra uzduotis: pakoreguoti kieki -isminusuoti countInStock
+    let newChart = [...items]
+    let product = newChart.find( x => x.name === item.name);
+    console.log(product);
+
+    if(product===undefined && item.countInStock > 0){
+      product= {
+        ...item,
+        quantity: 1
+      }
+       newChart.push(product);
+       console.log(newChart)
+    } else if(product && product.quantity < item.countInStock){
+      //jei tokia preke yra
+      product.quantity++
+    } else {
+      alert("Prekes kiekis nepakankamas...")
+    }
+    setItems(newChart);
   }
    
   return (
