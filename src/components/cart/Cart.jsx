d7a9ed0b141a8bef6e24ac4775400358a4d14425
jkunciene <j.kunciene@gmail.com>
2023-02-20 21:27:36 +0200

    krepselis

---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Cart = (props) => {
+    console.log(props);
+  return (
+    <div>
+      krepselis
+    </div>
+  )
+}
+
+export default Cart
--- src/components/products/Products.jsx
+++ src/components/products/Products.jsx
@@ -1,18 +1,19 @@
 import Product from '../product/Product';
 import { useState } from 'react';
+import Cart from '../cart/Cart';
 
 const Products = () => {
   const [items, setItems] = useState([]);
 
   const addToCart = (item) => {
-    console.log('funkcija addToCart', item);
+    console.log('tikrinu, kokia preke ateina, ant kurios paspausta', item);
     // jei tokios prekes dar nera krepselyje -> sukurk savybe kiekis 1
     //jei tokia preke jau yra -> didink kieki +1
     //jei prekes likutis pakankamas
     //extra uzduotis: pakoreguoti kieki -isminusuoti countInStock
     let newChart = [...items]
     let product = newChart.find( x => x.name === item.name);
-    console.log(product);
+    console.log("tikrinu ar tokia preke yra krepselyje",product);
 
     if(product===undefined && item.countInStock > 0){
       product= {
@@ -20,7 +21,7 @@ const Products = () => {
         quantity: 1
       }
        newChart.push(product);
-       console.log(newChart)
+       console.log("spausdinu krepseli po push() metodo ", newChart)
     } else if(product && product.quantity < item.countInStock){
       //jei tokia preke yra
       product.quantity++
@@ -33,6 +34,7 @@ const Products = () => {
   return (
     <div>
       <Product addToCart={addToCart}/>
+      <Cart items={items}/>
     </div>
   )
 }
